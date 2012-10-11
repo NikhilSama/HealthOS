7e1076789010729e1776c17615660da74e528e4a
Bharani M <bharani91@gmail.com>
2012-10-11 12:00:48 +0530

    Initial Commit

---
+++ javascripts/base.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+  $(document).foundationTopBar();
+  for(var i=1; i <= 4; i++) {
+    $('.share_options_' + i).PieMenu({
+      'starting_angel':0,
+      'angel_difference' : 90,
+      'radius':100,
+    });
+  }
+
+});
+
